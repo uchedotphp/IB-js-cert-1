af9b99875f569c067d0c24077e9cbf5d352d678d
Uchechuwku Prince Nwulu <uchedotphp@gmail.com>
2024-08-15 01:20:10 +0100

    FEAT: Move single note template from the html file into a variable in js

---
+++ index.js
@@ -0,0 +1,13 @@
+// HTML note template
+const noteTemplate = `
+    <div class="note">
+        <div class="noteHeader">
+            <p class="noteIndex">1</p>
+        </div>
+        <input type="text" placeholder="Add title" />
+        <textarea name="" id="" placeholder="Add text"></textarea>
+        <div class="noteBtns">
+            <button id="delete">Delete</button>
+        </div>
+    </div>
+    `;
