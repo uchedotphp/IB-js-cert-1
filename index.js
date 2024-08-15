a80d6a5ffc10a62ef2377f0f3cd511678fc722d8
Uchechuwku Prince Nwulu <uchedotphp@gmail.com>
2024-08-15 08:55:00 +0100

    The following actions were carried out: 1. Wrap the whole JS logic in a DOMContentLoaded event listener. 2. Create a click event listener to cater for most click events; majorly the events that will not be available on DOM loaded state. 3. Create a function to delete a note and reorder the note index.

--- index.js
+++ index.js
@@ -1,42 +1,65 @@
-// HTML note template
-const noteTemplate = (noteNumber) => `
-    <div class="note">
-        <div class="noteHeader">
-            <p class="noteIndex">${noteNumber}</p>
+document.addEventListener("DOMContentLoaded", () => {
+  // HTML note template
+  const noteTemplate = (noteNumber) => `
+        <div class="note">
+            <div class="noteHeader">
+                <p class="noteIndex">${noteNumber}</p>
+            </div>
+            <input type="text" placeholder="Add title" />
+            <textarea name="" id="" placeholder="Add text"></textarea>
+            <div class="noteBtns">
+                <button id="delete">Delete</button>
+            </div>
         </div>
-        <input type="text" placeholder="Add title" />
-        <textarea name="" id="" placeholder="Add text"></textarea>
-        <div class="noteBtns">
-            <button id="delete">Delete</button>
-        </div>
-    </div>
-    `;
+        `;
 
-// update note count
-function updateNoteCount() {
-  const totalNotes = document.querySelectorAll(".notesContainer .note").length;
-  const notesCountNumContainer = document.querySelector(".notesCountNum");
-  notesCountNumContainer.textContent = totalNotes;
-}
-
-// Add Note
-function addNote() {
   const notesContainer = document.querySelector(".notesContainer");
-  if (notesContainer) {
-    const lastNoteIndex =
-      (notesContainer.querySelectorAll(".note") &&
-        notesContainer.querySelectorAll(".note").length) ||
-      0;
-    notesContainer.innerHTML += noteTemplate(lastNoteIndex + 1);
+  const addNoteBtn = document.getElementById("add");
+
+  // update note count
+  function updateNoteCount() {
+    const totalNotes = notesContainer.querySelectorAll(".note").length;
+    const notesCountNumContainer = document.querySelector(".notesCountNum");
+    notesCountNumContainer.textContent = totalNotes;
   }
-}
 
-const addNoteBtn = document.getElementById("add");
-if (addNoteBtn) {
-  addNoteBtn.addEventListener("click", () => {
-    addNote();
+  // Add Note
+  function addNote() {
+    if (notesContainer) {
+      const lastNoteIndex =
+        (notesContainer.querySelectorAll(".note") &&
+          notesContainer.querySelectorAll(".note").length) ||
+        0;
+      notesContainer.innerHTML += noteTemplate(lastNoteIndex + 1);
+    }
+  }
+
+  // Delete a note
+  function deleteNote(note) {
+    note.remove();
+    // Get all remaining notes
+    const notesLeft = notesContainer.querySelectorAll(".note");
+    // Reorder the note numbers
+    notesLeft.forEach((note, index) => {
+      note.querySelector(".noteIndex").textContent = index + 1;
+    });
     updateNoteCount();
-  });
-}
+  }
 
-updateNoteCount(); // called once on dom loaded
+  addNoteBtn &&
+    addNoteBtn.addEventListener("click", () => {
+      addNote();
+      updateNoteCount();
+    });
+
+  // Event listeners
+  notesContainer.addEventListener("click", (event) => {
+    // Deleting a note
+    if (event.target.id === "delete") {
+      const noteToDelete = event.target.closest(".note");
+      deleteNote(noteToDelete);
+    }
+  });
+
+  updateNoteCount(); // called once on dom loaded
+});
